61d816036853c520e51faf3c13a848dd43df78eb
unknown <erdematik8@gmail.com>
2022-12-07 18:37:10 +0300

    edited matrix multiplication

--- WGS84-ED50-convertor/app.js
+++ WGS84-ED50-convertor/app.js
@@ -7,27 +7,25 @@ Math.radians = function(degrees) {
 	return degrees * Math.PI / 180;
 }
 
-console.log(Math.radians(-0.4738/3600));
-
-const  matrix1 = [[1, Math.radians(-0.4738/3600), Math.radians(-0.0003/3600)],[Math.radians(0.4738/3600), 1 ,Math.radians(-0.0183/3600)],
-[Math.radians(0.0003/3600) , Math.radians(0.0183/3600), 1 ]]
-
-console.log(typeof(Math.radians(-0.4738/3600)));
 
 
-const matrix2 = [3869416.913,2830423.6819,4192997.6984]
-console.log((1*3869416.913)+Math.radians(-0.4738/3600)*2830423.6819+ Math.radians(-0.0003/3600)*4192997.6984);
-console.log(matrix1);
+
+const  matrixAlt =[[1,Math.radians(0.4738/3600),Math.radians(0.0003/3600)], [Math.radians(-0.4738/3600),1,Math.radians(0.0183/3600)],
+[Math.radians(-0.0003/3600),Math.radians(-0.0183/3600),1]]
+
+
+const matrix2 = [3869416.913,2830423.682,4192997.6984]
+
+
+let allFixed = matrixAlt.map(el=>
+    el.map((e)=>e.toFixed(20)))
+
+console.log(allFixed);
 console.log(matrix2);
-const conParamtr = math.matrix(matrix1)
 
+const conParamtr = math.matrix(allFixed)
 const convPara2 = math.matrix(matrix2)
 let result = math.multiply(conParamtr,convPara2)
-
-
-
-
-
 
 console.log(result._data);
 
